488549b7f283534e10a8cb5c4b448d8c05cf231e
Jay_ Muzima👩‍💻 <ishimwejean15@gmail.com>
2026-02-11 11:28:17 +0200

    Updated Hero component with fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import './Hero.css'
-import arrow_right from '../../assets/arrow_right.svg'
+import React from "react";
+import "./Hero.css";
+import arrow_right from "../../assets/arrow_right.svg";
 
 const Hero = () => {
   return (
-    <div className='hero container'>
-        <div className="hero-text">
-
-
-          <h1>We Ensure better education for a better warld</h1>
-          <p>Our cutting-esge carriculm is designed to empower students
-              with the knowledge , skills, and experiences needed to excel in 
-              the dynamic field of education</p>
-          <button className='btn'>Exprole more <img src={arrow_right} alt=""/></button>
-        </div>
-        
-      
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We Ensure better education for a better warld</h1>
+        <p>
+          Our cutting-esge carriculm is designed to empower students with the
+          knowledge , skills, and experiences needed to excel in the dynamic
+          field of education
+        </p>
+        <button className="btn">
+          Exprole more by checking whether is working{" "}
+          <img src={arrow_right} alt="" />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
